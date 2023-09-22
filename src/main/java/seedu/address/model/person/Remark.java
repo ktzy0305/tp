da558ad3a6249a45311a8d0b643b55a5b4f79d8c
ktzy0305 <kevintoh0305@gmail.com>
2023-09-23 00:26:59 +0800

    Model: Add Remark class

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,40 @@
+package seedu.address.model.person;
+
+public class Remark {
+
+    public final String value;
+
+    /**
+     * Constructs an {@code Remark}.
+     *
+     * @param remark A valid remark.
+     */
+    public Remark(String remark) {
+        value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Remark)) {
+            return false;
+        }
+
+        Remark otherRemark = (Remark) other;
+        return value.equals(otherRemark.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
